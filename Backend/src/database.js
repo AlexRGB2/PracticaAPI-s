6e252905951bfb7731a75edaa3041e5592e2a820
AlexRGB <alexismtz200326@gmail.com>
2022-11-06 19:21:03 -0600

    MONGODB

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -1,20 +1,5 @@
 import mongoose from "mongoose";
 
-const url = `mongodb+srv://AlexRGB:4J72AZAzCxnnZJE@cluster0.hm1sfft.mongodb.net/?retryWrites=true&w=majority`;
-const connectionParams = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-//Coneccion CLOUD DATABASE
-// mongoose
-//   .connect(url, connectionParams)
-//   .then(() => {
-//     console.log("DB Connect");
-//   })
-//   .catch((err) => {
-//     console.error(`Error connecting to the database. n${err}`);
-//   });
 
 //Descomentar para coneccion local
 mongoose
